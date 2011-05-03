c5d120fe6b044ea0245167b7d6ad589e3a65304c
Steve <madklowns@gmail.com>
2011-05-03 06:10:37 -0400

    Adding in the models for tic tac toe

--- tictactoe/models.py
+++ tictactoe/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+#import datetime
 
-# Create your models here.
+
+class Game(models.Model):
+    name = models.CharField(max_length=200)
+
+class Player(models.Model):
+#    def __unicode__(self):
+#        return self.choice
+    game = models.ForeignKey(Game)
+    name = models.CharField(max_length=200)
+
+class Move(models.Model):
+    character = models.CharField(max_length=2)
+
+class GameBoard(models.Model):
+#    def __unicode__(self):
+#        return self.question
+#
+#    def was_published_today(self):
+#        return self.pub_date.date() == datetime.date.today()
+
+    x = models.IntegerField()
+    y = models.IntegerField()
+    move = models.ForeignKey(Move)
+    game = models.ForeignKey(Game)
