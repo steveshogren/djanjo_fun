5a0eedac51a1a58896514adea60e8b7c22adf3af
Steve <madklowns@gmail.com>
2011-05-08 21:15:30 -0400

    Adding in more of the jetbrains xml attributes, basically cleaning to the most minimal for copy and paste

--- jetbrainschemes/models.py
+++ jetbrainschemes/models.py
@@ -1,21 +1,65 @@
-from xml.dom import minidom
-
 class ReadXmlToPhpColors():
     def parse(self):
-        colors = {'text_back': 'ffffff', 'text_fore': 'ffffff', 'keyword_fore': 'ffffff'}
-        colors['keyword_fore'] = self.Lookup("PHP_KEYWORD", "FOREGROUND")
-        colors['text_back'] = self.Lookup("TEXT", "BACKGROUND")
-        colors['text_fore'] = self.Lookup("TEXT", "FOREGROUND")
-        return colors
+        
+        xmlOptions = ["PHP_KEYWORD", "TEXT", "PHP_VAR", "PHP_OPERATION_SIGN", "PHP_DOC_COMMENT_ID",
+                     "PHP_DOC_COMMENT_ID", "PHP_DOC_COMMENT_ID", "PHP_DOC_TAG", "PHP_COMMENT", "PHP_COMMENT",
+                     "PHP_IDENTIFIER", "PHP_STRING"]
+
+        cssString = ""
+        for xmlOption in xmlOptions:
+            cssString += self.ConvertToCSS(xmlOption)
+        return cssString
+
+    def ConvertToCSS(self, value):
+        css = "." + value + " { "
+        foreground = self.Lookup(value, "FOREGROUND")
+        if foreground:
+            if len (foreground) == 4:
+                foreground = "00" + foreground
+            css += "color: #" + foreground + ";"
+        back = self.Lookup(value, "BACKGROUND")
+        if back:
+            css += "background: #" + back + ";"
+        style = self.ConvertFontStyle(self.Lookup(value, "FONT_TYPE"))
+        if style:
+            css += "font-style: " + style + ";"
+        decoration = self.ConvertTextDecoration(self.Lookup(value, "EFFECT_TYPE"))
+        if decoration:
+            css += "text-decoration: " + decoration + ";"
+        css += " } \n"
+        return css
+
+    def ConvertFontStyle(self, id):
+        styleText = ""
+        if id:
+            id = int(id)
+            if id == 1:
+                styleText = "bold"
+            if id == 2:
+                styleText = "italic"
+
+        return styleText
+
+    def ConvertTextDecoration(self, id):
+        styleText = ""
+        if id:
+            id = int(id)
+            if id == 1:
+                styleText = "underline"
+
+        return styleText
 
     def Lookup(self, firstName, secondName):
-        xmldoc = minidom.parse('jetbrainschemes/test.xml')
+#        xmldoc = minidom.parse('jetbrainschemes/test.xml')
+        xmldoc = minidom.parse('jetbrainschemes/emacs.xml')
         for node in xmldoc.getElementsByTagName("option"):
             if node.getAttribute("name") == firstName:
                 children = node.childNodes[1]
                 for node in children.getElementsByTagName("option"):
                     if node.getAttribute("name") == secondName:
                         return node.getAttribute("value")
+
+from xml.dom import minidom
 
 test = ReadXmlToPhpColors()
 print test.parse()
--- jetbrainschemes/views.py
+++ jetbrainschemes/views.py
@@ -3,14 +3,6 @@ from django.shortcuts import render_to_response
 from jetbrainschemes.models import ReadXmlToPhpColors
 
 def index(request):
-#    game_list = Game.objects.all()[:5]
-#    read = ReadXmlToPhpColors()
-#    xml = read.parse()
-#    return render_to_response("colors/index.html", {'xml': xml})
     read = ReadXmlToPhpColors()
-    xml = read.parse()
-    return render_to_response('colors/index.html', {
-        'php_keyword': xml['keyword_fore'],
-        'text_back': xml['text_back'],
-        'text_fore': xml['text_fore'],
-    })
+    colors = read.parse()
+    return render_to_response('colors/index.html', {'colors': colors})
