5528df73296bdbfa49865dea1b7cbf63ff4b55b2
Steve <madklowns@gmail.com>
2011-07-24 21:07:27 -0400

    Added a selector for choosing any of the existing schemes

--- jetbrainschemes/views.py
+++ jetbrainschemes/views.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.http import HttpResponse
 import settings
 from django.shortcuts import render_to_response
@@ -5,13 +6,23 @@ from django.template.context import RequestContext
 from jetbrainschemes.models import XmlToPersistence, CssSettings
 
 def index(request):
-    file_name = 'twilightest.xml'
+    selected_scheme = 'twilightest.xml'
 
 #    xmlToPersistence = XmlToPersistence()
 #    xmlToPersistence.convert(file_name)
 
-    c = CssSettings.objects.get(file_name=file_name)
+    if request.method == 'POST':
+        form = dropdownform(request.POST)
+        if form.is_valid():
+            selected_scheme = form.cleaned_data['dropdown']
 
+    c = CssSettings.objects.get(file_name=selected_scheme)
+
+    x = dropdownform(initial={"dropdown": c.id})
     return render_to_response('colors/index.html',
-                              {'colors': c.css, 'test': settings.MEDIA_ROOT, 'mediaurl': settings.MEDIA_URL},
+                              {'color_schemes': selected_scheme, 'colors': c.css, 'selector': x, 'test': settings.MEDIA_ROOT, 'mediaurl': settings.MEDIA_URL},
                               context_instance=RequestContext(request))
+
+
+class dropdownform(forms.Form):
+    dropdown = forms.ModelChoiceField(queryset = CssSettings.objects.all(), empty_label="(Nothing)")
